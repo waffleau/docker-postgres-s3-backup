0e91e989e64fb1f5979417e700f0bae5c6ff20ef
Matt Ellis <m.ellis27@gmail.com>
2016-08-26 20:16:52 +1000

    Changes to hopefully fix backup faliures due to gzip not liking overwriting files

--- backup.sh
+++ backup.sh
@@ -3,13 +3,16 @@ set -e
 
 echo "Creating backup of database: ${POSTGRES_DB}"
 
+export BACKUP_NAME=$(date +"%Y-%m-%d-T%H%M%SZ")
+export BACKUP_FILE=$BACKUP_NAME.sql
+
 export PGPASSWORD=$POSTGRES_PASSWORD
-pg_dump -h $POSTGRES_HOST -p $POSTGRES_PORT -d $POSTGRES_DB -U $POSTGRES_USER -f /backups/dump.sql
-gzip /backups/dump.sql
+
+pg_dump -h $POSTGRES_HOST -p $POSTGRES_PORT -d $POSTGRES_DB -U $POSTGRES_USER -f /backups/$BACKUP_FILE
+gzip -f /backups/$BACKUP_FILE
 
 echo "Uploading backup to S3 bucket: ${S3_BUCKET}/${S3_PREFIX}"
 
-export BACKUP_FILE=$(date +"%Y-%m-%d-T%H%M%SZ").sql.gz
-cat /backups/dump.sql.gz | aws s3 cp - s3://$S3_BUCKET/$S3_PREFIX/$BACKUP_FILE
+cat /backups/$BACKUP_FILE.gz | aws s3 cp - s3://$S3_BUCKET/$S3_PREFIX/$BACKUP_FILE.gz
 
 echo "Backup uploaded successfully: ${BACKUP_FILE}"
